46756f58769a6083f4d7bf7a1a41fd41337b24ce
victor braz <victor.90braz@gmail.com>
2024-01-05 12:00:03 +0100

    useState to zero

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(10);
+  const [count, setCount] = useState(0);
 
   const increment = () => {
     setCount((prevCount) => prevCount + 1);
